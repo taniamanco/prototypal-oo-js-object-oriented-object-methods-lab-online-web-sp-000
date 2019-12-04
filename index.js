6d3b48ff5c066455ca8b38121460b30877745cb1
Tania Manco <belalov.tania16@gmail.com>
2019-12-04 16:34:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function BoardMember(name, homeState, training){
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+}
+
+BoardMember.prototype.veto = function(){
+  return "No, I must disagree"
+}
+
+BoardMember.prototype.approve = function(){
+  return "You can do that!"
+}
+
+BoardMember.prototype.doCharity = function(){
+  return "I like to help people"
+}
